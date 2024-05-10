aa178f814da3753c6d25b91491357fb0635d9539
R3LM <118556218+R3LM15@users.noreply.github.com>
2024-05-09 23:14:06 -0500

    Tarea Individual #1

---
+++ Tarea_Letras/Mateo_Molina_Moises_Estrella/Tarea_Indi_#1/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args){
+        Letrafor lf;
+
+
+
+        lf= new Letrafor();
+        lf.lm();
+    
+    }
+}
---
+++ Tarea_Letras/Mateo_Molina_Moises_Estrella/Tarea_Indi_#1/src/Letrafor.java
@@ -0,0 +1,32 @@
+public class Letrafor {
+
+    int   n = 10;
+    String letra = "x";
+    String vacio = " ";
+
+    public  void lm() {
+
+
+        for(int i=1; i<=n; i++){
+            System.out.print(letra);
+            System.out.print(letra);
+    
+                for(int a=1; a<=i; a++)
+                System.out.print(vacio);
+                System.out.print(letra);
+                System.out.print(letra);
+    
+                for(int a=1; a<=n*2-2*i; a++)
+                System.out.print(vacio);
+                System.out.print(letra);
+                System.out.print(letra);
+    
+                for(int a=1; a<=i; a++)
+                System.out.print(vacio);
+                System.out.print(letra);
+               
+            System.out.println(letra);   
+    
+    }
+}
+}
