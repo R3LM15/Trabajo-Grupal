22b86e3aa03ad92f95b9ee8e09b3c46ff1c0ec3b
R3LM <118556218+R3LM15@users.noreply.github.com>
2024-05-26 00:39:13 -0500

    series v0.1

--- PoliRetos_01/PoliRetos/src/App.java
+++ PoliRetos_01/PoliRetos/src/App.java
@@ -2,6 +2,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         int mmTamano= 10;
 
+        
+
         System.out.println("      GRUPO #03      ");
         System.out.println("---- INTEGRANTES ----");
         System.out.println("->  Molina.Mateo");
@@ -10,14 +12,35 @@ public class App {
         System.out.println("->  ");
         System.out.println("---- ----------- ----");
                     System.out.println(" ");
-       
-
-        MolinaMateo SN1;
+                    
+                    
+        System.out.println(">> Series numéricas");
+                        System.out.println(" ");
+            MolinaMateo SN1;
                 SN1= new MolinaMateo();
                 SN1.mmSN1(mmTamano);
-                    System.out.println(" ");
+                        System.out.println(" ");
+
+            MolinaMateo SN7;
+                SN7= new MolinaMateo();
+                SN7.mmSN7(mmTamano);
+                        System.out.println(" ");
+
+            MolinaMateo SN8;
+                SN8= new MolinaMateo();
+                SN8.mmSN8(mmTamano);
+                        System.out.println(" ");
+
+            MolinaMateo SN9;
+                SN9= new MolinaMateo();
+                SN9.mmSN9(mmTamano);
+                        System.out.println(" ");
+
+            MolinaMateo SN10;
+                SN10= new MolinaMateo();
+                SN10.mmSN10(mmTamano);
+                        System.out.println(" ");
 
 
-        System.out.println("Hello, RELM TEST 1");
     }
 }
--- PoliRetos_01/PoliRetos/src/MolinaMateo.java
+++ PoliRetos_01/PoliRetos/src/MolinaMateo.java
@@ -1,11 +1,52 @@
 public class MolinaMateo {
     
-    public void mmSN1( int mmTamano){
+    public void mmSN1(int mmTamano){
+        System.out.print("SN1:  ");
 
-        for(int i=0; i<=mmTamano; i++){
-           System.out.print(i+i);
-           System.out.print(" ");
+            for(int i=0; i<=mmTamano; i++){
+                System.out.print(i);
+                System.out.print(" ");
+      }
 
+    }
+    public void mmSN7(int mmTamano){
+        System.out.print("SN7:  ");
+        int mminicio = 1;
+        
+        for (int i = 0; i < mmTamano; i++) {
+            System.out.print(mminicio + " ");
+            mminicio = mminicio + 3;
+                 
+      }
+    }  
+    public void mmSN8(int mmTamano){
+        System.out.print("SN8:  ");
+        int mminicio = 3;
+        
+        for (int i = 0; i < mmTamano; i++) {
+            System.out.print(mminicio + " ");
+            mminicio = mminicio + 5;
+                 
+      }
+    }  
+    public void mmSN9(int mmTamano){
+        System.out.print("SN9:  ");
+        int mminicio = 2;
+        
+        for (int i = 0; i < mmTamano; i++) {
+            System.out.print(mminicio + " ");
+            mminicio = mminicio * 2;
+                 
+      }
+    }
+    public void mmSN10(int mmTamano){
+        System.out.print("SN10: ");
+        int mminicio = 3;
+        
+        for (int i = 0; i < mmTamano; i++) {
+            System.out.print(mminicio + " ");
+            mminicio = mminicio * 3;
+                 
       }
     }
 
