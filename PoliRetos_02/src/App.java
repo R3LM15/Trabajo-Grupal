c68528cc52b915dd8bf8d538aeda9b3875936f3e
R3LM <118556218+R3LM15@users.noreply.github.com>
2024-06-13 19:38:46 -0500

    poliretos 2da parte

---
+++ PoliRetos_02/src/App.java
@@ -0,0 +1,17 @@
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+       
+
+        PoliRetos.integrantes();
+        PoliRetos.CadenaCaracteres();
+        PoliRetos.Arrays();
+        PoliRetos.Loading();
+        PoliRetos.Recursion();
+        
+
+
+
+    }
+}
---
+++ PoliRetos_02/src/PoliRetos.java
@@ -0,0 +1,72 @@
+public class PoliRetos {
+    String frase;
+
+
+
+    public static void integrantes() {
+        System.out.println("      GRUPO #03      ");
+        System.out.println("---- INTEGRANTES ----");
+        System.out.println("->  Molina.Mateo");
+        System.out.println("->  Morales.Francisco");
+        System.out.println("->  Zamira.Jimenez");
+        System.out.println("->  Menendez.Joshua  ");
+        System.out.println("->  Marcillo.Marco");
+        System.out.println("---- ----------- ----");
+        System.out.println(" ");
+
+    }
+    public static void CadenaCaracteres() {
+
+        System.out.println(">> Cadena de caracteres:");
+     
+
+    
+    }    
+
+    public static void Arrays() {
+        System.out.println("      GRUPO #03      ");
+    }
+
+    public static void Loading() {
+        System.out.println("      GRUPO #03      ");
+    }
+
+    public static void Recursion() {
+        System.out.println("      GRUPO #03      ");
+    }
+
+
+
+
+
+
+    public String getFrase() {
+        return frase;
+    }
+
+    public void setFrase(String frase) {
+        if (frase == null)
+            this.frase = " ";
+        else
+            this.frase = frase;
+       
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
